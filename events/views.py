befb69ff422daf6031d5f25e7d0c9f3add08e72b
Isaacn123 <nsambai72@gmail.com>
2024-04-19 19:59:10 +0300

    categories

--- events/views.py
+++ events/views.py
@@ -99,11 +99,9 @@ def delete_event(request, pk):
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def create_category(request):
-    print(f"DATAFG_before: {request.data}")
-    get_category = EventCategory.objects.filter(category_name = request.data['category_name']).first()
     # return Response({"data": get_category})
     if request.method == "POST":
-        print(f"DATAFG: {request.data}")
+        # print(f"DATAFG: {request.data}")
         data = request.data['category']
         get_category = EventCategory.objects.filter(category_name = data['category_name']).first()
         if get_category:
